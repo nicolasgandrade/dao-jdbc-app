fae41f8513e0a08e877ef6fb0f46b51e255c1942
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-01 16:28:00 -0300

    feat: add findById for Seller Dao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +11,9 @@ import java.util.Date;
 
     public static void main(String[] args) {
 
-        Department obj = new Department(1, "Books");
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
 
