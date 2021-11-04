54e4485ba1a92840406d5dca557747765068af43
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-04 20:39:03 -0300

    feat: implement insert seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -33,5 +33,11 @@ public class Program {
             System.out.println(obj);
         }
 
+        System.out.println();
+        System.out.println("==== TEST 4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id: " + newSeller.getId());
+
     }
 }
