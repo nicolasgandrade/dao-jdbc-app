ac8a5c211dde22002be6ef57cf205f53f5bdc731
Nicolas Guerrero <nicolasguerrero72@gmail.com>
2021-11-05 21:06:06 -0300

    feat: implement update and delete for department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program2 {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
@@ -25,11 +28,24 @@ public class Program2 {
             System.out.println(obj);
         }
 
+//        System.out.println();
+//        System.out.println("==== TEST 3: department insert ====");
+//        Department newDepartment = new Department(1, "Phones");
+//        departmentDao.insert(newDepartment);
+//        System.out.println("Inserted! New id: " + newDepartment.getId());
+
         System.out.println();
-        System.out.println("==== TEST 2: department insert ====");
-        Department newDepartment = new Department(1, "Phones");
-        departmentDao.insert(newDepartment);
-        System.out.println("Inserted! New id: " + newDepartment.getId());
+        System.out.println("==== TEST 4: department update ====");
+        Department dep = departmentDao.findById(2);
+        dep.setName("House automation");
+        departmentDao.update(dep);
+        System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 5: delete =======");
+        System.out.print("Enter id for delete test: ");
+        int id = sc.nextInt();
+        departmentDao.deleteById(id);
+        System.out.println("Delete completed");
 
     }
 }
